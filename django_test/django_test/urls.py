fc0662701912f1f63e7199c91d3c7080c224ac96
Adrian0910 <adriansantander60@aragon.unam.mx>
2022-07-28 00:10:53 -0500

    vista usando template html

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -5,11 +5,17 @@ from django.urls import path
 from django_test.views import saludo #importamos la funcion de views.py 
 from django_test.views import pan
 from django_test.views import dia
+from django_test.views import saludo_nombre
+from django_test.views import fecha_nacimiento
+from django_test.views import plantilla
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo, name='view_de_saludo'),
     path('pan/', pan, name='view_del_pan'),
     path('dia/', dia, name='view_del_dia'),
+    path('saludo-nombre/<str:nombre>', saludo_nombre, name='saludo_con_nombre'), #agregamos el parametro nombre directamente en la url
+    path('edad/<int:anio>', fecha_nacimiento, name='fecha_nacimiento'), #Calcular la edad
+    path('plantilla/', plantilla, name='plantilla'),
 ]
 
--- django_test/django_test/views.py
+++ django_test/django_test/views.py
@@ -1,5 +1,10 @@
+from calendar import calendar
 from django.http import HttpResponse
 from datetime import datetime
+
+from django.shortcuts import render
+
+
 def saludo(request):
     return HttpResponse("Hola mundo")
 
@@ -9,5 +14,15 @@ def pan(request):
 def dia(request):
     hoy = datetime.today()
     return HttpResponse(f'El dia de hoy es {hoy.strftime("%A, %d de %B de %Y")}')
+
+def saludo_nombre(request, nombre):
+    return HttpResponse(f'Hola {nombre}')
+
+def fecha_nacimiento(request, anio):
+    calEdad = datetime.today().year - int(anio)
+    return HttpResponse(f'Tu año de nacimiento fue el {calEdad}')
+
+def plantilla(request):
+    return render(request, 'plantilla.html', {'nombre': 'Juan'})
 
 
