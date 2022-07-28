a60a4dcde21853d731ca01cbf934176e8196b549
Adrian0910 <adriansantander60@aragon.unam.mx>
2022-07-27 23:30:57 -0500

    show date

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -4,9 +4,12 @@ from django.urls import path
 
 from django_test.views import saludo #importamos la funcion de views.py 
 from django_test.views import pan
+from django_test.views import dia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo, name='view_de_saludo'),
     path('pan/', pan, name='view_del_pan'),
+    path('dia/', dia, name='view_del_dia'),
 ]
+
--- django_test/django_test/views.py
+++ django_test/django_test/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-
+from datetime import datetime
 def saludo(request):
     return HttpResponse("Hola mundo")
 
 def pan(request):
     return HttpResponse("Pan de muerto")
+
+def dia(request):
+    hoy = datetime.today()
+    return HttpResponse(f'El dia de hoy es {hoy.strftime("%A, %d de %B de %Y")}')
+
 
